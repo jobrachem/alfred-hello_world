477825e33cf83d7ea7987cb99c59c65ef901983f
Johannes Brachem <jobrachem@posteo.de>
2020-03-26 11:09:30 +0100

    formatting

--- script.py
+++ script.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-from alfred import Experiment
-from alfred.page import Page
 import alfred.element as elm
 import alfred.section as sec
+from alfred import Experiment
+from alfred.page import Page
 
 
 class HelloWorld(Page):
     def on_showing(self):
-        hello_text = elm.TextEntryElement('Please enter some text.')
+        hello_text = elm.TextEntryElement("Please enter some text.")
         self.append(hello_text)
 
 
 def generate_experiment(self, config):
     exp = Experiment(config=config)
 
-    hello_world = HelloWorld(title='Hello, world!')
+    hello_world = HelloWorld(title="Hello, world!")
 
     main = sec.Section()
     main.append(hello_world)
